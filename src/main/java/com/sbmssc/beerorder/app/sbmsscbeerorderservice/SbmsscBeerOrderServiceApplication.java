85398dd0f24eac47e04c0728026cfa6ad05e9f80
yadu <yaduhat@gmail.com>
2020-07-12 00:24:33 -0500

    intial commit

---
+++ src/main/java/com/sbmssc/beerorder/app/sbmsscbeerorderservice/SbmsscBeerOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sbmssc.beerorder.app.sbmsscbeerorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsscBeerOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsscBeerOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbmssc/beerorder/app/sbmsscbeerorderservice/SbmsscBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbmssc.beerorder.app.sbmsscbeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsscBeerOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
